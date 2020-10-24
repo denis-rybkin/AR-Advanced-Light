2fb8a83dcafb60d413ab29f8801f740c4c163ff7
denrb <rybkindenu@gmail.com>
2020-10-24 22:00:44 +0300

    add interactivity

--- ARExample/ContentView.swift
+++ ARExample/ContentView.swift
@@ -9,12 +9,16 @@ import SwiftUI
 import RealityKit
 
 struct ContentView : View {
+    
+    @State private var offset: Float = 0
+    
     var body: some View {
         ZStack {
-            ARViewContainer().edgesIgnoringSafeArea(.all)
+            ARViewContainer(offset: $offset)
+                .edgesIgnoringSafeArea(.all)
             VStack {
                 Spacer()
-                Button("Test button", action: { })
+                Slider(value: $offset, in: -0.1...0.1)
                     .padding()
             }
         }
@@ -23,27 +27,36 @@ struct ContentView : View {
 
 struct ARViewContainer: UIViewRepresentable {
     
+    @Binding var offset: Float
+    
+    private let objectSize: Float = 0.03
+    
     func makeUIView(context: Context) -> ARView {
         let arView = ARView(frame: .zero)
         let anchor = AnchorEntity(plane: .horizontal)
         arView.scene.anchors.append(anchor)
         let material = SimpleMaterial(color: .gray, isMetallic: false)
-        let objectSize: Float = 0.03
         let object = MeshResource.generateSphere(radius: objectSize)
         let entity = ModelEntity(mesh: object, materials: [material])
         entity.transform.translation = [0, objectSize, 0]
         anchor.addChild(entity)
         
-        let lightObject = PointLight()
-        lightObject.light.intensity *= 0.01
-        lightObject.transform.translation = [0, objectSize * 3, 0]
-        anchor.addChild(lightObject)
+        let lightAnchor = AnchorEntity(plane: .horizontal)
+        arView.scene.anchors.append(lightAnchor)
+//        let lightObject = PointLight()
+        let lightObject = MeshResource.generateSphere(radius: objectSize / 4)
+//        lightObject.light.intensity *= 0.01
+//        lightObject.transform.translation = [offset, objectSize * 2, 0]
+        let lightEntity = ModelEntity(mesh: lightObject, materials: [material])
+        lightAnchor.addChild(lightEntity)
         
         return arView
-        
     }
     
-    func updateUIView(_ uiView: ARView, context: Context) {}
+    func updateUIView(_ uiView: ARView, context: Context) {
+        let anchor = uiView.scene.anchors[1]
+        anchor.transform.translation = [offset, 0, 0]
+    }
     
 }
 
