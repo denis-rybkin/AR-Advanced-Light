d497099d436b1cb61924e91c4e7db64704d3784e
denrb <rybkindenu@gmail.com>
2020-10-24 22:45:08 +0300

    need fix

--- ARExample/ContentView.swift
+++ ARExample/ContentView.swift
@@ -38,24 +38,24 @@ struct ARViewContainer: UIViewRepresentable {
         let material = SimpleMaterial(color: .gray, isMetallic: false)
         let object = MeshResource.generateSphere(radius: objectSize)
         let entity = ModelEntity(mesh: object, materials: [material])
-        entity.transform.translation = [0, objectSize, 0]
         anchor.addChild(entity)
         
         let lightAnchor = AnchorEntity(plane: .horizontal)
         arView.scene.anchors.append(lightAnchor)
-//        let lightObject = PointLight()
-        let lightObject = MeshResource.generateSphere(radius: objectSize / 4)
-//        lightObject.light.intensity *= 0.01
+        let lightObject = PointLight()
+        lightObject.light.intensity *= 0.01
+        // debug object
+//        let lightObject = MeshResource.generateSphere(radius: objectSize / 4)
 //        lightObject.transform.translation = [offset, objectSize * 2, 0]
-        let lightEntity = ModelEntity(mesh: lightObject, materials: [material])
-        lightAnchor.addChild(lightEntity)
+//        let lightEntity = ModelEntity(mesh: lightObject, materials: [material])
+        lightAnchor.addChild(lightObject)
         
         return arView
     }
     
     func updateUIView(_ uiView: ARView, context: Context) {
-        let anchor = uiView.scene.anchors[1]
-        anchor.transform.translation = [offset, 0, 0]
+        uiView.scene.anchors[0].transform.translation = [0, objectSize, 0] // need fix
+        uiView.scene.anchors[1].transform.translation = [offset, objectSize * 3, 0]
     }
     
 }
